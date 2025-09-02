37dc7529ecfdda92ee3989dbe8d9b7dc75ad4128
deekshitha7271 <deekshithapachigolla7271@gmail.com>
2025-09-02 12:24:16 +0530

    initial commit

---
+++ db/seed.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@/lib/generated/prisma";
+
+import sampleData from "./sample-data";
+
+
+//we are using async functions because prisma methods that we are going to use to get the products are asynchronous.
+async function main(){
+    const prisma = new PrismaClient();//initialize the prisma object
+    await prisma.product.deleteMany();
+
+    await prisma.product.createMany({data :sampleData.products})
+
+    console.log("Database seeded successfully")
+
+
+}
+main();
